a9c9de838efa0165d7b8eb8869274aba4bdf0acc
zeyang919 <920193796@qq.com>
2019-12-18 09:21:04 +0800

    自用版本 在使用时 将file_fr 参数的值改为法语助手所在地址即可

---
+++ Fr_restaurer_自用.py
@@ -0,0 +1,20 @@
+import os
+import socket
+import win32api
+import win32con
+
+# 获取当前主机名
+hostname = socket.gethostname()
+# 获取存储时间的文件
+file = 'C:/Users/' + hostname + '/AppData/Roaming/Francochinois/Frhelper/study.db'
+# 判断如果文件存在则删除
+if os.path.exists(file):
+    os.remove(file)
+# 存储注册表地址
+key = win32api.RegOpenKey(win32con.HKEY_CURRENT_USER, 'SOFTWARE\\Francophonie\\Frhelper\\Customer Info', 0,
+                          win32con.KEY_ALL_ACCESS)
+# 修改注册表地址
+win32api.RegSetValueEx(key, 'TimesLeft3', 0, win32con.REG_DWORD, 9999)
+# 打开法语助手
+file_fr = "D:/法语/法语助手/Frhelper/frhelper.exe"
+os.startfile(file_fr)
