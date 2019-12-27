b74bbfc2c60311ba8a5a01f79133eb5e960f4f2b
zeyang919 <920193796@qq.com>
2019-12-27 12:29:41 +0800

    更改获取 剩余时间 文件的方法

--- Fr_restaurer.py
+++ Fr_restaurer.py
@@ -1,15 +1,18 @@
 import os
-import socket
 import win32api
 import win32con
 
-# 获取当前主机名
-hostname = socket.gethostname()
-# 获取存储时间的文件
-file = 'C:/Users/' + hostname + '/AppData/Roaming/Francochinois/Frhelper/study.db'
-# 判断如果文件存在则删除
-if os.path.exists(file):
-    os.remove(file)
+try:
+    # 获取APPDATA地址
+    key_appdata = win32api.RegOpenKey(win32con.HKEY_CURRENT_USER, 'Volatile Environment', 0, win32con.KEY_READ)
+    path_appdata = win32api.RegQueryValueEx(key_appdata, "APPDATA")[0]
+    # 获取存储时间的文件
+    file = path_appdata + '\Francochinois\Frhelper\study.db'
+    # 判断如果文件存在则删除
+    if os.path.exists(file):
+        os.remove(file)
+except:
+    pass
 # 存储注册表地址
 key = win32api.RegOpenKey(win32con.HKEY_CURRENT_USER, 'SOFTWARE\\Francophonie\\Frhelper\\Customer Info', 0,
                           win32con.KEY_ALL_ACCESS)
